32b7b29d2b405316bcff35aed5dc9e8f4f1e5ef0
Kevin Furjan <kfurjan@gmail.com>
2019-08-01 20:03:57 +0200

    Proof of concept finished

--- util/helpers.py
+++ util/helpers.py
@@ -3,43 +3,46 @@ import requests
 import locale
 
 
-def getCityWeather(city):
+def getCityWeather(city, forecast=None):
     """
-    Get weather report for given city
-    :param city: Specify city for which to get weather
+    Get current or forecast weather report for given city
+    :param city: Specify city for which to get weather report
+    :param forecast: Param to use when forecast weather report is needed
     :return: Weather report if city is known
     """
-    free_OMW_API_key = "014daf06eddbe256673d2d86504c69d1"
-    cityWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?appid=" \
-                     + free_OMW_API_key + "&q=" + city
+    reportType = "weather"
+    if forecast is not None:
+        reportType = "forecast"
+
+    OMW_API_key = "014daf06eddbe256673d2d86504c69d1"
+    cityWeatherUrl = "http://api.openweathermap.org/data/2.5/" + reportType + "?appid=" \
+                     + OMW_API_key + "&q=" + city
 
     report = requests.get(cityWeatherUrl)
 
     if report.status_code == 200:
         return report.json()
 
+    # if city is unknown, spelling is wrong or server is unreachable
+    # return weather report as None object
     report = None
     return report
 
 
-def getDay(tomorrow=None, dayAfter=None):
+def getDay(daysFromNow=None):
     """
     Get name of the day
-    :param tomorrow: If name od tomorrow is needed
-    :param dayAfter: If name of day after tomorrow is needed
+    :param daysFromNow: How many days from current day
     :return: Name of the day based on current datetime
     """
-    # hard coded function to use english day names
+
+    # hard coded function to use english day names instead of
+    # language used within operating system's region settings
     locale.setlocale(locale.LC_ALL, 'en_US.utf8')
 
-    if tomorrow is not None:
+    if daysFromNow is not None:
         today = datetime.date.today()
-        tomorrow = today + datetime.timedelta(days=1)
-        return tomorrow.strftime("%A")
-
-    elif dayAfter is not None:
-        today = datetime.date.today()
-        tomorrow = today + datetime.timedelta(days=2)
-        return tomorrow.strftime("%A")
+        day = today + datetime.timedelta(days=int(daysFromNow))
+        return day.strftime("%A")
 
     return datetime.date.today().strftime("%A")
